c6e57f8905c2c863896aeafef8f1a5e9e126d06d
leona <leonardo.tissi.si@gmail.com>
2023-10-25 19:35:28 -0300

    scroll in column

--- app/src/main/java/br/com/leonardo/gardenguardian/ui/screens/homeScreen/homeScreen.kt
+++ app/src/main/java/br/com/leonardo/gardenguardian/ui/screens/homeScreen/homeScreen.kt
@@ -14,8 +14,11 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -27,8 +30,10 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Info
@@ -176,6 +181,7 @@ fun HomeScreen(homeScreenViewModel: HomeScreenViewModel = koinViewModel()) {
             modifier = Modifier
                 .fillMaxWidth()
                 .heightIn(400.dp, 450.dp)
+                .verticalScroll(rememberScrollState())
         ) {
 
             Box(
