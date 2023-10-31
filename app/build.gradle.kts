3fead74a025894ea8488044477a5bf7610931e7c
leona <leonardo.tissi.si@gmail.com>
2023-10-31 17:13:15 -0300

    splashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,7 +75,6 @@ dependencies {
     val lifecycleVersion = "2.6.2"
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
 
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
