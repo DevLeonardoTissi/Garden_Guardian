dc9ca0696bd43dc985b3cad154f46b09b29c2983
leona <leonardo.tissi.si@gmail.com>
2023-11-03 18:16:45 -0300

    Refactor

--- app/src/main/java/br/com/leonardo/gardenguardian/ui/components/components.kt
+++ app/src/main/java/br/com/leonardo/gardenguardian/ui/components/components.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -27,7 +26,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -43,24 +41,6 @@ import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.LottieConstants
 import com.airbnb.lottie.compose.rememberLottieComposition
-
-@Composable
-fun MyAlertDialog(
-    onDismissRequest: () -> Unit = {},
-    onConfirmation: () -> Unit = {},
-    dialogTitle: String,
-    dialogText: String,
-    icon: ImageVector
-) {
-    AlertDialog(
-        onDismissRequest = { onDismissRequest() },
-        confirmButton = { TextButton(onClick = { onConfirmation() }) { Text(text = ("confirm")) } },
-        title = { Text(text = dialogTitle) },
-        text = { Text(text = dialogText) },
-        icon = { Icon(icon, contentDescription = null) },
-        dismissButton = { TextButton(onClick = { onDismissRequest() }) { Text(text = "Dismiss") } },
-    )
-}
 
 
 @Composable
