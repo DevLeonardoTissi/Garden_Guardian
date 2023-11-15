4a326966b12c3260148210366eec33cf0b606a26
leona <leonardo.tissi.si@gmail.com>
2023-11-15 16:45:53 -0300

    adding option to enable and disable notifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 27
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
